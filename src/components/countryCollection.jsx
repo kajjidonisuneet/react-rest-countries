8f7fd75a4ba78316eb5725115b55b77144607dd2
kajjidonisuneet <kajjidonisuneet@gmail.com>
2022-09-07 20:47:02 +0530

    before adding css tailwind

--- src/components/countryCollection.jsx
+++ src/components/countryCollection.jsx
@@ -3,6 +3,7 @@ import CountryCard from "./countryCard";
 import axios from "axios";
 import Dropdown from "../common/dropdown";
 import SearchBox from "./searchBox";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const baseUrl = "https://restcountries.com/v2/region/asia";
 const baseUrl2 = "https://restcountries.com/v2/region/";
@@ -26,35 +27,41 @@ class CountryCollection extends Component {
   state = {
     countries: [],
     searchQuery: "",
+    loading: true,
+    cardsToDisplay: true,
   };
 
   async componentDidMount() {
     const { data: countries } = await axios.get(baseUrl);
-    this.setState({ countries });
+    this.setState({ countries, loading: false});
   }
 
   handleRegionChange = async (e) => {
     try {
+      this.setState({ loading: true, cardsToDisplay: true });
       const selectedRegion = e.target.value;
       const { data: countries } = await axios.get(baseUrl2 + selectedRegion);
-      this.setState({ countries });
+      this.setState({ countries, loading: false});
     } catch (error) {
-      console.log(error);
+      this.setState({ cardsToDisplay: false });
     }
   };
 
   updateSearchQuery = async (event) => {
     const searchQuery = event.target.value;
-    this.setState({ searchQuery });
+    this.setState({ searchQuery, cardsToDisplay: true });
   };
 
   getSearchData = async () => {
     try {
-      const { data: countries } = await axios.get(baseUrl3 + this.state.searchQuery);
-      this.setState({ countries });
+      this.setState({ loading: true, countries: [], cardsToDisplay: true });
+      const { data: countries } = await axios.get(
+        baseUrl3 + this.state.searchQuery
+      );
+      this.setState({ countries, loading: false, cardsToDisplay: true });
     } catch (error) {
       if (error.response && error.response.status === 404)
-        alert("No Movies. Please try other names")
+        this.setState({ loading: false, cardsToDisplay: false });
     }
   };
 
@@ -74,6 +81,21 @@ class CountryCollection extends Component {
             onChange={this.handleRegionChange}
           />
         </div>
+        {this.state.loading && (
+          <div>
+            <MagnifyingGlass
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="MagnifyingGlass-loading"
+              wrapperStyle={{}}
+              wrapperClass="MagnifyingGlass-wrapper"
+              glassColor="#c0efff"
+              color="#e15b64"
+            />
+          </div>
+        )}
+        {!this.state.cardsToDisplay && <div>No Data</div>}
         <div className="grid grid-cols-4 gap-4 p-10">
           {countries.map((c) => (
             <CountryCard countryData={c} key={c.alpha3Code} />
